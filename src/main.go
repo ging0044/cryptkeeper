a24bad1765ac55622534b88747ab64296507b922
Patrick Gingras <ging0044@algonquinlive.com>
2018-02-12 15:22:38 -0500

    added logging middleware, added environment variable PORT

--- src/main.go
+++ src/main.go
@@ -5,11 +5,27 @@ import (
 
 	"goji.io"
 	"goji.io/pat"
+	"os"
+	"log"
+	"github.com/unrolled/logger"
 )
 
+var port string
+
+func init() {
+	port = os.Getenv("PORT")
+	if port == "" {
+		log.Printf("No PORT environment variable set, using 8080...")
+		port = "8080"
+	}
+}
+
 func main() {
+	loggerMiddleware := logger.New()
+
 	//init mux
 	mux := goji.NewMux();
+	mux.Use(loggerMiddleware.Handler)
 
 	mux.Handle(pat.Get("/public/*"), http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 	mux.Handle(pat.Get("/user/*"), userMux)
@@ -17,5 +33,6 @@ func main() {
 	mux.Handle(pat.Get("/holdings/*"), holdingMux)
 	mux.Handle(pat.Get("/*"), homeMux)
 
-	http.ListenAndServe(":8080", mux)
+	log.Printf("Listening on port %v", port)
+	http.ListenAndServe(":"+port, mux)
 }
